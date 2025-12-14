802d9a6eda844c8d35053dc52fb46ee93b563547
CapoShip <capoship@gmail.com>
2025-12-14 18:04:17 -0500

    probleme affichage

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // <--- C'est ici que ça change (avant c'était juste 'tailwindcss')
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
